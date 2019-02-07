1b94363dafe0c8433b5ce68bfef20976164ef768
Dloft <derek.lofton1@gmail.com>
2019-02-07 14:35:49 -0500

    IMDB

--- liri.js
+++ liri.js
@@ -1,20 +1,42 @@
-//At the top of the `liri.js` file, add code to read and set any environment variables with the dotenv package:
+// //At the top of the `liri.js` file, add code to read and set any environment variables with the dotenv package:
 
-require("dotenv").config();
+// require("dotenv").config();
 
-//Add the code required to import the `keys.js` file and store it in a variable.
-  var keys = require("./keys.js");
-
-  //You should then be able to access your keys information like so
-  var spotify = new Spotify(keys.spotify);
+// //Add the code required to import the `keys.js` file and store it in a variable.
+//   var keys = require("./keys.js");
 
 
-  //9. Make it so liri.js can take in one of the following commands:
+//   //9. Make it so liri.js can take in one of the following commands:
 
-//    * `concert-this`
+// //    * `concert-this`
 
-//    * `spotify-this-song`
+// //    * `spotify-this-song`
+// var spotify = new Spotify(keys.spotify);
 
 //    * `movie-this`
 
+var axios = require("axios");
+
+var movieName = process.argv[2];
+
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+axios.get(queryUrl).then(
+    function(response) {
+        console.log("Title: " + response.data.Title);
+      console.log("Release Year: " + response.data.Year);
+      console.log("Rating: " + response.data.Rated);
+      console.log("Rating: " + response.data.imdbRating);
+      console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
+    }
+  );
+//   * Title of the movie.
+//   * Year the movie came out.
+//   * IMDB Rating of the movie.
+//   * Rotten Tomatoes Rating of the movie.
+//   * Country where the movie was produced.
+//   * Language of the movie.
+//   * Plot of the movie.
+//   * Actors in the movie.
+  
 //    * `do-what-it-says`
+
