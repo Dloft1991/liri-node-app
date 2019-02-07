bc3736ca48c6d83f63762ac81dbc61ee496b95ad
Dloft <derek.lofton1@gmail.com>
2019-02-07 17:34:50 -0500

    spoty

--- liri.js
+++ liri.js
@@ -11,7 +11,68 @@
 // //    * `concert-this`
 
 // //    * `spotify-this-song`
-// var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
+var movieName = process.argv[2];
+
+
+actionsSpeak(action, argument);
+
+function actionsSpeak (action, argument) {
+    argument = userInput();
+
+    switch (action) {
+        case "spotify-this-song":
+
+        var songTitle = argument;
+
+        if (songTitle === "") {
+            lookupSpecificSong();
+        }else {
+            SongInfo(songTitle);
+        }
+        break;
+        
+    }
+}
+
+function SongInfo(songTitle) {
+    spofity.search({type: "track", query: songTitle}, function(err, data) {
+        if (err) {
+            console.log(err);
+            return
+        }
+        var artistArray = data.tracks.items[0].album.artists;
+        var artistName = [];
+
+        for (var i = 0; i < artistArray.length; i++) {
+            artistName.push(artistArray[i].name);
+        }
+
+        var artist = artistName.join(", ");
+
+        console.log("Artist: " + artist);
+        console.log("Song: " + data.tracksitems[0].name);
+        console.log("Spotify Preview URL: " + artdata.tracks.items[0].preview_url);
+        console.log("Album Name: " + data.tracks.items[0].album.name);
+    });
+}
+
+function lookupSpecificSong() {
+
+	
+	spotify.lookup({type: 'track', id: '3c3407938f7248e4bdece5862d743366'}, function(err, data) {
+		if (err) {
+			logOutput.error(err);
+			return
+		}
+
+	
+		console.log("Artist: " + data.artists[0].name);
+		console.log("Song: " + data.name);
+		console.log("Spotify Preview URL: " + data.preview_url);
+		console.log("Album Name: " + data.album.name);
+	});
+}
 
 //    * `movie-this`
 
@@ -27,10 +88,10 @@ axios.get(queryUrl).then(
       console.log("Rating: " + response.data.Rated);
       console.log("Rating: " + response.data.imdbRating);
       console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
-      console.log("Rating: " + response.data.Country);
-      console.log("Rating: " + response.data.Language);
-      console.log("Rating: " + response.data.Plot);
-      console.log("Rating: " + response.data.Actors);
+      console.log("Country: " + response.data.Country);
+      console.log("Language: " + response.data.Language);
+      console.log("Plot: " + response.data.Plot);
+      console.log("Actors: " + response.data.Actors);
     }
   );
 //   * Title of the movie.
